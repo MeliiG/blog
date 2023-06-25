b09c91401d8087fe4126fe0ea5893290a4ad0232
MeliiG <Meli.bogar15@gmail.com>
2023-06-24 21:18:10 -0500

    temas y login falso

--- js/main.js
+++ js/main.js
@@ -66,4 +66,54 @@ $(document).ready(function(){
     // <a href="#" class="botonmas">leer más</a>
     // </article>
 
+    var thema = $("#tema")
+    $(topink).click(function(){
+        thema.attr("href","css/pink.css")
+    });
+
+    $(topurple).click(function(){
+        thema.attr("href","css/purple.css")
+    });
+
+    $(toblue).click(function(){
+        thema.attr("href","css/blue.css")
+    });
+
+
+
+//scroll subir
+
+$('.subir').click(function(e){
+    e.preventDefault();
+    $('html,body').animate({
+        scrollTop:0
+    },500);
+    return false;
+})
+
+//falselogin
+
+$("#login form").submit(function(){
+    var form_name = $("#form_name").val();
+
+    localStorage.setItem("form_name", form_name);
+
+});
+
+var form_name = localStorage.getItem("form_name");
+
+if(form_name != null && form_name != "undefined"){
+    var about_parrafo = $("#about p");
+
+    about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong> ");
+    about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+
+    $("#login").hide();
+
+    $("#logout").click(function(){
+        localStorage.clear();
+        location.reload();
+    });
+
+}
 })
